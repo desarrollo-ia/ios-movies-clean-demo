f37ffa78ace8a95d5886e7c758bbf700a2ed7796
Eduardo Pacheco <jpacheco@ia.com.mx>
2019-09-03 17:24:45 -0500

    STEP 2: Worker and Use Cases

--- CLEANMovies/Modules/Movie List scene/MoviesUseCases.swift
+++ CLEANMovies/Modules/Movie List scene/MoviesUseCases.swift
@@ -6,5 +6,37 @@
 //  Copyright (c) 2019 IA Interactive. All rights reserved.
 //
 
+import Foundation
+
+// MARK: - Module Error Types
+enum MovieListError: Error {
+    case networkError
+    case unknown
+}
+
+// MARK: - Module use cases
 enum Movies {
+
+    enum GetMovieList {
+
+        struct Request {
+            let listType: MovieListType
+        }
+
+        struct Response {
+            let movies: [Movie]
+        }
+
+        struct ViewModel {
+            let movieCells: [MovieCellViewModel]
+        }
+
+        // MARK: - Internal Types
+        struct MovieCellViewModel {
+            let posterURL: URL?
+            let titleLabel: String
+            let dateLabel: String
+            let scoreLabel: String
+        }
+    }
 }
--- CLEANMovies/Workers/MoviesWorker.swift
+++ CLEANMovies/Workers/MoviesWorker.swift
@@ -6,5 +6,21 @@
 //  Copyright (c) 2019 IA Interactive. All rights reserved.
 //
 
+import Moya
+import RxSwift
+
 class MoviesWorker {
+
+    // MARK: - Properties
+    let provider = APIProvider<MoviesAPI>(stub: true)
+
+    // MARK: - Public Methods
+    func getMovieList(type: MovieListType) -> Single<[Movie]> {
+        let request = provider.rx.request(.moviesList(type: .popular))
+            .filterSuccessfulStatusCodes()
+            .map(MovieListResponse.self)
+            .map({ $0.movieList })
+
+        return request
+    }
 }
